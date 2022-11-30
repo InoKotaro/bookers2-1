b252c4636821863780915eefb885e712a80f573d
EC2 Default User <ec2-user@ip-172-31-31-191.ap-northeast-1.compute.internal>
2022-11-30 07:32:32 +0000

    update

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,15 @@
 class SearchesController < ApplicationController
-    efore_action :authenticate_user!
+    before_action :authenticate_user!
 
   def search
     @range = params[:range]
 
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
+      render :search_result
     else
       @books = Book.looks(params[:search], params[:word])
+      render :search_result
     end
   end
 end
